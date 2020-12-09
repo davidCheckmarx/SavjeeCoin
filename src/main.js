65050c7a45030249e04266bd8b8314270c1dad32
DavidCohen <davidc@checkmarx.com>
2020-12-09 22:56:06 +0200

    Change structure

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.genKeyPair();
+
+// From that we can calculate your public key (which doubles as your wallet address)
+const myWalletAddress = myKey.getPublic('hex');
+
+// Create new instance of Blockchain class
+const savjeeCoin = new Blockchain();
+
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 100);
+tx1.signTransaction(myKey);
+savjeeCoin.addTransaction(tx1);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+tx2.signTransaction(myKey);
+savjeeCoin.addTransaction(tx2);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(`Balance of xavier is ${savjeeCoin.getBalanceOfAddress(myWalletAddress)}`);
+
+// Uncomment this line if you want to test tampering with the chain
+// savjeeCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log('Blockchain valid?', savjeeCoin.isChainValid() ? 'Yes' : 'No');
